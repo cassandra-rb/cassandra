c2dcbb6ef869c33c668a96d2db141cc20d0036e8
Evan Weaver <eweaver@twitter.com>
2009-06-30 04:45:40 -0700

    First commit.

---
+++ cassandra_client.rb
@@ -0,0 +1,156 @@
+require 'rubygems'
+require 'thrift'
+require 'json'
+
+$LOAD_PATH << "gen-rb"
+require 'cassandra'
+
+class CassandraClient
+  attr_reader :table, :client, :transport, :schema
+
+  # Instantiate a new CassandraClient and open the connection.
+  def initialize(table, host, port = 9160, block_for = 1)
+    socket = Thrift::Socket.new(host, port)
+    @transport = Thrift::BufferedTransport.new(socket)
+    protocol = Thrift::BinaryProtocol.new(@transport)    
+    @client = Cassandra::Client.new(protocol)    
+    
+    @transport.open    
+    @table, @block_for = table, block_for
+    @schema = @client.describeTable(@table)
+  end
+  
+  ## Write
+  
+  # Insert a row for a key. Pass a flat hash for a regular column family, and 
+  # a nested hash for a super column family.
+  def insert(column_family, key, hash, timestamp = Time.now.to_i)
+    insert = is_super(column_family) ? :insert_super : :insert_standard
+    send(insert, column_family, key, hash, timestamp)
+  end
+  
+  private
+
+  def insert_standard(column_family, key, hash, timestamp = Time.now.to_i)
+    mutation = Batch_mutation_t.new(
+      :table => @table, 
+      :key => key, 
+      :cfmap => {column_family => hash_to_columns(hash, timestamp)})
+    @client.batch_insert(mutation, @block_for)
+  end 
+
+  def insert_super(column_family, key, hash, timestamp = Time.now.to_i)
+    mutation = Batch_mutation_super_t.new(
+      :table => @table, 
+      :key => key, 
+      :cfmap => {column_family => hash_to_super_columns(hash, timestamp)})
+    @client.batch_insert_superColumn(mutation, @block_for)
+  end 
+  
+  public
+  
+  ## Delete
+  
+  # Remove the element at the column_family:key:super_column:column 
+  # path you request.
+  def remove(column_family, key, super_column = nil, column = nil, timestamp = Time.now.to_i)
+    column_family += ":#{super_column}" if super_column
+    column_family += ":#{column}" if column
+    @client.remove(@table, key, column_family, timestamp, @block_for )
+  end   
+  
+  ## Read
+
+  # Count the elements at the column_family:key:super_column path you 
+  # request.
+  def count(column_family, key, super_column = nil)
+    column_family += ":#{super_column}" if super_column
+    @client.get_column_count(@table, key, column_family)
+  end
+ 
+  # Return a list of single values for the elements at the
+  # column_family:key:super_column:column path you request.
+  def get_columns(column_family, key, super_columns, columns = nil)
+    get_slice_by_names = (is_super(column_family) && !columns) ? :get_slice_super_by_names : :get_slice_by_names
+    if super_columns and columns
+      column_family += ":#{super_columns}" 
+      columns = Array(columns)
+    else
+      columns = Array(super_columns)
+    end
+        
+    hash = columns_to_hash(@client.send(get_slice_by_names, @table, key, column_family, columns))
+    columns.map { |column| hash[column] }
+  end
+        
+  # Return a hash or single value representing the element at the 
+  # column_family:key:super_column:column path you request.
+  def get(column_family, key, super_column = nil, column = nil, limit = 100)
+    column_family += ":#{super_column}" if super_column
+    column_family += ":#{column}" if column    
+    
+    # You have got to be kidding
+    if is_super(column_family)
+      if column
+        @client.get_column(@table, key, column_family).value
+      elsif super_column
+        columns_to_hash(@client.get_superColumn(@table, key, column_family).columns)
+      else
+        columns_to_hash(@client.get_slice_super(@table, key, "#{column_family}:", -1, limit))
+      end
+    else
+      if super_column
+        @client.get_column(@table, key, column_family).value
+      elsif is_sorted_by_time(column_family)
+        columns_to_hash(@client.get_columns_since(@table, key, column_family, 0))
+      else
+        columns_to_hash(@client.get_slice(@table, key, "#{column_family}:", -1, limit))
+      end 
+    end
+  rescue NotFoundException
+    is_super(column_family) && !column ? {} : nil
+  end  
+
+  # Return a list of keys in the column_family you request. Requires the
+  # table to be partitioned with OrderPreservingHash.
+  def get_key_range(column_family, key_range = ''..'', limit = 100)
+    @client.get_key_range(@table, Array(column_family), key_range.begin, key_range.end, limit)
+  end
+  
+  private
+  
+  def is_super(column_family)
+    @schema[column_family.split(':').first]['type'] == 'Super'
+  rescue NoMethodError
+    raise "Invalid column_family #{column_family}"
+  end
+  
+  def is_sorted_by_time(column_family)
+    @schema[column_family.split(':').first]['sort'] == 'Time'
+  end
+  
+  def columns_to_hash(columns)
+    columns = Array(columns)
+    Hash[*flatten_once(columns.map do |c| 
+      c.is_a?(SuperColumn_t) ? [c.name, columns_to_hash(c.columns)] : [c.columnName, c.value]
+    end)]
+  end  
+  
+  def hash_to_columns(hash, timestamp)
+    hash.map do |column, value|
+      Column_t.new(:columnName => column, :value => value, :timestamp => timestamp)
+    end    
+  end
+  
+  def hash_to_super_columns(hash, timestamp)
+    hash.map do |super_column, columns|
+      SuperColumn_t.new(:name => super_column, :columns => hash_to_columns(columns, timestamp))
+    end
+  end
+    
+  def flatten_once(array)
+    result = []
+    array.each { |el| result.concat(el) }
+    result
+  end   
+end
---
+++ cassandra_test.rb
@@ -0,0 +1,156 @@
+
+require 'test/unit'
+require 'cassandra_client'
+require 'ruby-debug'
+
+class CassandraClientTest < Test::Unit::TestCase
+  def setup
+    @c = CassandraClient.new('Twitter', '127.0.0.1')  
+    @c.get_key_range('Statuses').each { |key| @c.remove('Statuses', key) }
+    @c.get_key_range('StatusRelationships').each { |key| @c.remove('StatusRelationships', key) }
+  end
+
+  def test_get_key_time_sorted
+    @c.insert('Statuses', key, {'body' => 'v', 'user' => 'v'})
+    assert_equal({'body' => 'v', 'user' => 'v'}, @c.get('Statuses', key))
+    assert_equal({}, @c.get('Statuses', 'bogus'))
+  end
+
+  def test_get_key_name_sorted
+    @c.insert('Users', key, {'body' => 'v', 'user' => 'v'})
+    assert_equal({'body' => 'v', 'user' => 'v'}, @c.get('Users', key))
+    assert_equal({}, @c.get('Users', 'bogus'))
+  end
+
+  def test_get_value
+    @c.insert('Statuses', key, {'body' => 'v'})
+    assert_equal 'v', @c.get('Statuses', key, 'body')
+    assert_nil @c.get('Statuses', 'bogus', 'body')
+  end
+  
+  def test_get_super_key
+    @c.insert('StatusRelationships', key, {'user_timelines' => {'4' => 'v', '5' => 'v'}})
+    assert_equal({'user_timelines' => {'4' => 'v', '5' => 'v'}}, @c.get('StatusRelationships', key))
+    assert_equal({}, @c.get('StatusRelationships', 'bogus'))
+  end
+
+  def test_get_super_key_multi
+    @c.insert('StatusRelationships', key, {
+      'user_timelines' => {'1' => 'v1'}, 
+      'mentions_timelines' => {'2' => 'v2'}})
+    assert_equal({
+      'user_timelines' => {'1' => 'v1'}, 
+      'mentions_timelines' => {'2' => 'v2'}}, @c.get('StatusRelationships', key))
+    assert_equal({}, @c.get('StatusRelationships', 'bogus'))
+  end
+
+  def test_get_super_sub_key
+    @c.insert('StatusRelationships', key, {'user_timelines' => {'4' => 'v', '5' => 'v'}})
+    assert_equal({'4' => 'v', '5' => 'v'}, @c.get('StatusRelationships', key, 'user_timelines'))
+    assert_equal({}, @c.get('StatusRelationships', 'bogus', 'user_timelines'))
+  end
+  
+  def test_get_super_value
+    @c.insert('StatusRelationships', key, {'user_timelines' => {'1' => 'v'}})
+    assert_equal('v', @c.get('StatusRelationships', key, 'user_timelines', '1'))
+    assert_nil @c.get('StatusRelationships', 'bogus', 'user_timelines', '1')
+  end  
+  
+  def test_get_key_range
+    @c.insert('Statuses', '3', {'body' => 'v'})
+    @c.insert('Statuses', '4', {'body' => 'v'})
+    @c.insert('Statuses', '5', {'body' => 'v'})
+    assert_equal(['3', '4', '5'], @c.get_key_range('Statuses', '3'..'5'))
+  end
+
+  # Not supported
+  #  def test_get_key_range_super
+  #    @c.insert('StatusRelationships', '3', {'user_timelines' => {'1' => 'v'}})
+  #    @c.insert('StatusRelationships', '4', {'user_timelines' => {'1' => 'v'}})
+  #    @c.insert('StatusRelationships', '5', {'user_timelines' => {'1' => 'v'}})
+  #    assert_equal(['3', '4', '5'], @c.get_key_range('StatusRelationships', '3'..'5', 'user_timelines'))
+  #  end
+  
+  def test_remove_key
+    @c.insert('Statuses', key, {'body' => 'v'})
+    @c.remove('Statuses', key)
+    assert_equal({}, @c.get('Statuses', key))
+  end
+
+  def test_remove_value
+    @c.insert('Statuses', key, {'body' => 'v'})
+    @c.remove('Statuses', key, 'body')
+    assert_nil @c.get('Statuses', key, 'body')    
+  end
+
+  def test_remove_super_key
+    @c.insert('StatusRelationships', key, {'user_timelines' => {'1' => 'v'}})
+    @c.remove('StatusRelationships', key)
+    assert_equal({}, @c.get('StatusRelationships', key))
+  end
+
+  def test_remove_super_sub_key
+    @c.insert('StatusRelationships', key, {'user_timelines' => {'1' => 'v'}})
+    @c.remove('StatusRelationships', key, 'user_timelines')
+    assert_equal({}, @c.get('StatusRelationships', key, 'user_timelines'))
+  end
+
+  def test_remove_super_value
+    @c.insert('StatusRelationships', key, {'user_timelines' => {'1' => 'v'}})
+    @c.remove('StatusRelationships', key, 'user_timelines', '1')
+    assert_nil @c.get('StatusRelationships', key, 'user_timelines', '1')    
+  end
+
+  def test_insert_key
+    @c.insert('Statuses', key, {'body' => 'v', 'user' => 'v'})
+    assert_equal({'body' => 'v', 'user' => 'v'}, @c.get('Statuses', key))  
+  end
+
+  def test_insert_super_key
+    @c.insert('StatusRelationships', key, {'user_timelines' => {'1' => 'v', key => 'v'}})
+    assert_equal({'1' => 'v' , key => 'v'}, @c.get('StatusRelationships', key, 'user_timelines'))  
+  end
+  
+  def test_get_column_values
+    @c.insert('Statuses', key, {'body' => 'v1', 'user' => 'v2'})
+    assert_equal(['v1' , 'v2'], @c.get_columns('Statuses', key, ['body', 'user']))
+  end  
+
+  def test_get_column_values_super
+    @c.insert('StatusRelationships', key, {
+      'user_timelines' => {'1' => 'v1'}, 
+      'mentions_timelines' => {'2' => 'v2'}})
+    assert_equal [{'1' => 'v1'}, {'2' => 'v2'}], 
+      @c.get_columns('StatusRelationships', key, ['user_timelines', 'mentions_timelines'])
+  end  
+
+  # Not supported
+  #  def test_get_columns_super_sub
+  #    @c.insert('StatusRelationships', key, {
+  #      'user_timelines' => {'1' => 'v1'}, 
+  #      'mentions_timelines' => {'2' => 'v2'}})
+  #    assert_equal ['v1', 'v2'], 
+  #      @c.get_columns('StatusRelationships', key, 'user_timelines', ['1', key])
+  #  end    
+  
+  def test_count_keys
+    @c.insert('Statuses', key, {'body' => 'v1', 'user' => 'v2'})
+    assert_equal 2, @c.count('Statuses', key)
+  end 
+
+  def test_count_super_keys
+    @c.insert('StatusRelationships', key, {
+      'user_timelines' => {'1' => 'v1'}, 
+      'mentions_timelines' => {'2' => 'v2'}})
+    assert_equal 2, @c.count('StatusRelationships', key)
+  end 
+
+  def test_count_super_sub_keys
+    @c.insert('StatusRelationships', key, {'user_timelines' => {'1' => 'v1', key => 'v2'}})
+    assert_equal 2, @c.count('StatusRelationships', key, 'user_timelines')
+  end
+  
+  def key
+    caller.first[/`(.*?)'/, 1]
+  end
+end
