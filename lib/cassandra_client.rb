4cc12a56a175fc19f84ed8985d22e3775fb94a21
Evan Weaver <eweaver@twitter.com>
2009-07-04 03:35:17 -0700

    Relocate ordered hash.

--- lib/cassandra_client.rb
+++ lib/cassandra_client.rb
@@ -8,7 +8,7 @@ HERE = File.expand_path(File.dirname(__FILE__))
 require "#{HERE}/cassandra_client/client"
 require "#{HERE}/cassandra_client/table"
 require "#{HERE}/cassandra_client/serialization"
-require "#{HERE}/../vendor/ordered_hash"
+require "#{HERE}/cassandra_client/ordered_hash"
 
 $LOAD_PATH << "#{HERE}/../vendor/gen-rb"
 require 'cassandra'
