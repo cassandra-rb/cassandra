2bd6982db78f06f2de977ce1a9ef5a1f5e9f61b9
Evan Weaver <eweaver@twitter.com>
2009-07-03 17:13:24 -0700

    Revert "Update cassandra. Shit is broke. Thrift bindings didn't change."
    This reverts commit 1fae29ea110f38a7aa7feee53a742a2b7ed28f3d.

--- quickstart.sh
+++ quickstart.sh
@@ -1,9 +1,9 @@
 
-if [ ! -e vendor/cassandra-r791040 ]; then
+if [ ! -e vendor/cassandra-r789419 ]; then
   cd vendor
-  tar xjvf cassandra-r791040.tar.bz2
+  tar xjvf cassandra-r789419.tar.bz2
   cd ..
 fi
 
 export CASSANDRA_INCLUDE=`pwd`/conf/cassandra.in.sh
-vendor/cassandra-r791040/bin/cassandra -f
+vendor/cassandra-r789419/bin/cassandra -f
