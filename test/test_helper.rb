e5f69c20d6ce33c1894beb99e943f34c6862d4de
Gregory Man <man.gregory@gmail.com>
2009-08-09 19:54:13 +0300

    Stupid typo fix

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -8,5 +8,5 @@ begin
   @test_client = Cassandra.new('Twitter', '127.0.0.1')
 rescue Thrift::TransportException => e
   #TODO Server autorun
-  raise "Make sure that cassandra's server is runing. You can start it by running rake start_cassandra"  if e.message =~ /Could not connect/
+  raise "Make sure that cassandra's server is runing. You can start it by running rake cassandra"  if e.message =~ /Could not connect/
 end
