a302e539ef337e67d3880394d49474ac45836ba4
Evan Weaver <eweaver@twitter.com>
2009-08-09 08:22:59 -0700

    Delete ./quickstart.sh, since it's now in the Rakefile. Apparently no one notices shell scripts anymore.

--- quickstart.sh
+++
@@ -1,9 +0,0 @@
-
-if [ ! -e cassandra ]; then
-  rm -rf data
-  tar xjf vendor/cassandra.tar.bz2
-fi
-
-cd cassandra && ant && cd ..
-
-env CASSANDRA_INCLUDE=`pwd`/conf/cassandra.in.sh cassandra/bin/cassandra -f
