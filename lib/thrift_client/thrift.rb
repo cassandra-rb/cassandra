e29316c20300681bd3dde4ebb0540e8e1a738aea
Evan Weaver <eweaver@twitter.com>
2009-10-19 16:14:20 -0700

    Forgot a file.

---
+++ lib/thrift_client/thrift.rb
@@ -0,0 +1,22 @@
+
+module Thrift
+  class BufferedTransport
+    def timeout=(timeout)
+      @transport.timeout = timeout
+    end
+  
+    def timeout
+      @transport.timeout
+    end
+  end
+  
+  module Client
+    def timeout=(timeout)
+      @iprot.trans.timeout = timeout
+    end
+  
+    def timeout
+      @iprot.trans.timeout
+    end
+  end
+end
