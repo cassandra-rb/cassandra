a57cdb0555f90f663eb16b4f710bfdfa3b464621
Evan Weaver <eweaver@twitter.com>
2009-10-26 11:36:46 -0700

    Port tests to test/unit; clean up constant handling.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,5 +2,6 @@
 require 'test/unit'
 $LOAD_PATH << "#{File.expand_path(File.dirname(__FILE__))}/../lib"
 require 'thrift_client'
+require 'thrift_client/simple'
 require 'scribe'
 begin; require 'ruby-debug'; rescue LoadError; end
