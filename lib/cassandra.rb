a2129e83c871cb36285d2462bcd9d11cb34fe252
b <b@b3k.us>
2010-06-07 14:40:00 -0700

    handle multiple versions

--- lib/cassandra.rb
+++ lib/cassandra.rb
@@ -1,11 +1,19 @@
 require 'rubygems'
+gem 'thrift', '< 0.2.5'
+require 'thrift'
 require 'thrift_client'
 require 'json' unless defined?(JSON)
 require 'simple_uuid'
+
 here = File.expand_path(File.dirname(__FILE__))
 
-$LOAD_PATH << "#{here}/../vendor/gen-rb"
-require "#{here}/../vendor/gen-rb/cassandra"
+class Cassandra ; end
+unless Cassandra.methods.include?("VERSION")
+  require "#{here}/cassandra/0.6"
+end
+
+$LOAD_PATH << "#{here}/../vendor/#{Cassandra.VERSION}/gen-rb"
+require "#{here}/../vendor/#{Cassandra.VERSION}/gen-rb/cassandra"
 
 $LOAD_PATH << "#{here}"
 
@@ -16,9 +24,11 @@ require 'cassandra/comparable'
 require 'cassandra/long'
 require 'cassandra/ordered_hash'
 require 'cassandra/columns'
-require 'cassandra/protocol'
-require 'cassandra/cassandra'
+require "cassandra/#{Cassandra.VERSION}/protocol"
+require "cassandra/#{Cassandra.VERSION}/cassandra"
+unless Cassandra.VERSION.eql?("0.6")
+  require "cassandra/#{Cassandra.VERSION}/column_family"
+  require "cassandra/#{Cassandra.VERSION}/keyspace"
+end
 require 'cassandra/constants'
-require 'cassandra/column_family'
-require 'cassandra/keyspace'
 require 'cassandra/debug' if ENV['DEBUG']
