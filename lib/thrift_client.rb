01e7e181a464cd62bd28546efc6f8fdbedbd8dd4
Ryan King <ryan@theryanking.com>
2010-09-07 11:10:11 -0700

    fix retries documentation

--- lib/thrift_client.rb
+++ lib/thrift_client.rb
@@ -31,7 +31,7 @@ Valid optional parameters are:
 <tt>:randomize_server_list</tt>:: Whether to connect to the servers randomly, instead of in order. Defaults to <tt>true</tt>.
 <tt>:exception_classes</tt>:: Which exceptions to catch and retry when sending a request. Defaults to <tt>[IOError, Thrift::Exception, Thrift::ProtocolException, Thrift::ApplicationException, Thrift::TransportException, NoServersAvailable]</tt>
 <tt>:raise</tt>:: Whether to reraise errors if no responsive servers are found. Defaults to <tt>true</tt>.
-<tt>:retries</tt>:: How many times to retry a request. Defaults to the number of servers defined.
+<tt>:retries</tt>:: How many times to retry a request. Defaults to 0.
 <tt>:server_retry_period</tt>:: How many seconds to wait before trying to reconnect after marking all servers as down. Defaults to <tt>1</tt>. Set to <tt>nil</tt> to retry endlessly.
 <tt>:server_max_requests</tt>:: How many requests to perform before moving on to the next server in the pool, regardless of error status. Defaults to <tt>nil</tt> (no limit).
 <tt>:timeout</tt>:: Specify the default timeout in seconds. Defaults to <tt>1</tt>.
