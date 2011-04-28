8af2f987e5abf317fa99eaebfed401e379deb9fd
Robert Jackson <robertj@promedicalinc.com>
2011-04-27 23:07:26 -0400

    Use sub_column as SlicePredicate for SCF.

--- lib/cassandra/0.8/protocol.rb
+++ lib/cassandra/0.8/protocol.rb
@@ -51,11 +51,12 @@ class Cassandra
     def _multiget(column_family, keys, column, sub_column, count, start, finish, reversed, consistency)
       # Single values; count and range parameters have no effect
       if is_super(column_family) and sub_column
-        predicate = CassandraThrift::SlicePredicate.new(:column_names => [column])
+        predicate = CassandraThrift::SlicePredicate.new(:column_names => [sub_column])
         column_parent = CassandraThrift::ColumnParent.new(:column_family => column_family, :super_column => column)
         column_hash = multi_sub_columns_to_hash!(column_family, client.multiget_slice(keys, column_parent, predicate, consistency))
 
-        keys.inject({}){|hash, key| hash[key] = column_hash[key][column]; hash}
+        klass = sub_column_name_class(column_family)
+        keys.inject({}){|hash, key| hash[key] = column_hash[key][klass.new(sub_column)]; hash}
       elsif !is_super(column_family) and column
         predicate = CassandraThrift::SlicePredicate.new(:column_names => [column])
         column_parent = CassandraThrift::ColumnParent.new(:column_family => column_family)
